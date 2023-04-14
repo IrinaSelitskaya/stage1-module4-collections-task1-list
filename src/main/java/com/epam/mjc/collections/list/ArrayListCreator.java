7d15e9e55c98056a64e612786094aebf73d7f548
IrinaSelitskaya <silya@tut.by>
2023-04-14 23:52:16 +0300

    Updated ArrayListCreator.java, LinkedListCreator.java and ListSorter.java

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String>arrayList=new ArrayList<>();
+        for (int i=1;i<=sourceList.size();i++){
+            if (i%3==0){
+                arrayList.add(sourceList.get(i));
+                arrayList.add(sourceList.get(i));
+            }
+        }
+        return arrayList;
     }
 }
